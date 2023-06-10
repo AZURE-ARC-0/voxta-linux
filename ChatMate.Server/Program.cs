7fa75536093c7580fe70c1bc68344675a4dd7ee4
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-06-09 23:52:17 -0400

    Add a sample TCP server

---
+++ ChatMate.Server/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+public class Program
+{
+    private static readonly ILogger Logger = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        })
+        .CreateLogger<Program>();
+
+    public static async Task Main()
+    {
+        using var cts = new CancellationTokenSource();
+        var ongoingTasks = new ConcurrentDictionary<Task, byte>();
+
+        var listener = new TcpListener(IPAddress.Loopback, 5384);
+        listener.Start();
+        Logger.LogInformation("Server started: 0.0.0.0:5384");
+
+        Console.CancelKeyPress += (sender, args) =>
+        {
+            Logger.LogInformation("Shutting down...");
+            listener.Stop();
+            cts.Cancel();
+            args.Cancel = true;
+        };
+
+        while (!cts.Token.IsCancellationRequested)
+        {
+            try
+            {
+                var client = await listener.AcceptTcpClientAsync(cts.Token);
+                var clientProcessingTask = ProcessClient(client, Guid.NewGuid(), cts.Token);
+                ongoingTasks.TryAdd(clientProcessingTask, 0);
+                _ = clientProcessingTask.ContinueWith(t => ongoingTasks.TryRemove(t, out _));
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+
+        await Task.WhenAll(ongoingTasks.Keys);
+    }
+
+
+    private static async Task ProcessClient(TcpClient client, Guid clientId, CancellationToken cancellationToken)
+    {
+        using (client)
+        {
+            try
+            {
+                Logger.LogInformation($"Client {clientId} connected...");
+                var stream = client.GetStream();
+
+                await SendJson(stream, new Message { Type = "success", Content = "Connection established" }, cancellationToken);
+
+                byte[] buffer = ArrayPool<byte>.Shared.Rent(1024);
+                try
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        int bytesRead = await stream.ReadAsync(buffer.AsMemory(0, buffer.Length), cancellationToken);
+                        if (bytesRead == 0)
+                            break;
+
+                        var clientMessage = JsonSerializer.Deserialize<Message>(buffer.AsMemory(0, bytesRead).Span);
+
+                        if (clientMessage?.Type == "chat")
+                        {
+                            await SendJson(stream, new Message { Type = "info", Content = "Waiting" }, cancellationToken);
+                            // Simulate processing
+                            await Task.Delay(2000, cancellationToken);
+                            await SendJson(stream, new Message { Type = "chat", Content = "Message" }, cancellationToken);
+                        }
+                    }
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(buffer);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Logger.LogInformation($"Client {clientId} processing cancelled.");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Error while processing client {clientId}.");
+            }
+        }
+    }
+
+    private static async Task SendJson(NetworkStream stream, Message message, CancellationToken cancellationToken)
+    {
+        var json = JsonSerializer.SerializeToUtf8Bytes(message);
+        await stream.WriteAsync(json.AsMemory(), cancellationToken);
+    }
+}
+
+public class Message
+{
+    public required string Type { get; set; }
+    public required string Content { get; set; }
+}
