7246b37bc72b8a3bc1d81ed5985b16e0c3254562
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-06-13 13:09:51 -0400

    Abandoned refactor, will move to websockets

--- ChatMate.Server/ChatMateConnection.cs
+++ ChatMate.Server/ChatMateConnection.cs
@@ -1,6 +1,6 @@
 ﻿using System.Buffers;
-using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,6 +24,11 @@ public class ChatMateConnectionFactory
 
 public class ChatMateConnection
 {
+    private const int _bufferSize = 1024;
+    
+    private static readonly byte[] EventPrefix = "event: "u8.ToArray();
+    private static readonly byte[] DataPrefix = "data: "u8.ToArray();
+    
     private readonly ILogger<ChatMateConnection> _logger;
     private readonly ITextGenService _textGen;
     private readonly ITextToSpeechService _speechGen;
@@ -44,7 +49,7 @@ public class ChatMateConnection
     public async Task ProcessClientAsync(TcpClient client, CancellationToken cancellationToken)
     {
         _logger.BeginScope("Client {ClientId}", _clientId);
-        
+
         using (client)
         {
             try
@@ -52,84 +57,81 @@ public class ChatMateConnection
                 _logger.LogInformation("Client connected");
                 var stream = client.GetStream();
 
-                var buffer = ArrayPool<byte>.Shared.Rent(1024);
+                var buffer = ArrayPool<byte>.Shared.Rent(_bufferSize);
+                var lineBuffer = new MemoryStream();
+                string? currentEvent = null;
                 try
                 {
                     while (!cancellationToken.IsCancellationRequested && client.Connected)
                     {
-                        var bytesRead = await stream.ReadAsync(buffer.AsMemory(0, buffer.Length), cancellationToken);
+                        var bytesRead = await stream.ReadAsync(buffer, cancellationToken);
                         if (bytesRead == 0)
                             break;
-                        if (bytesRead == buffer.Length)
-                        {
-                            _logger.LogError("Buffer too small");
-                            break;
-                        }
 
-                        var memory = buffer.AsMemory(0, bytesRead);
+                        var processedBytes = 0;
 
-                        if (memory.Span.StartsWith("GET "u8))
+                        while (processedBytes < bytesRead)
                         {
-                            var proxy = _proxyHandlerFactory.Create(memory.Span, stream);
-                            _logger.LogInformation("HTTP Request {ProxyMethod} {ProxyPath}", proxy.Method, proxy.Uri.AbsolutePath);
-                            try
+                            var lineBreakPosition = Array.IndexOf(buffer, (byte)'\n', processedBytes, bytesRead - processedBytes);
+
+                            if (lineBreakPosition >= 0) // If a line break is found
                             {
-                                if (proxy is { Method: "GET", Uri.Segments: [_, "speech/", _] })
+                                lineBuffer.Write(buffer, processedBytes, lineBreakPosition - processedBytes);
+
+                                var line = lineBuffer.GetBuffer().AsMemory(0, (int)lineBuffer.Length);
+
+                                if (line.Span.StartsWith(EventPrefix))
                                 {
-                                    await _speechGen.HandleSpeechProxyRequestAsync(proxy);
+                                    currentEvent = Encoding.ASCII.GetString(line[EventPrefix.Length..].Span).Trim();
                                 }
-                                else
+                                else if (line.Span.StartsWith(DataPrefix) && currentEvent != null)
                                 {
-                                    await proxy.WriteTextResponseAsync(HttpStatusCode.NotFound, "Route Not Found");
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                await proxy.WriteTextResponseAsync(HttpStatusCode.InternalServerError, ex.Message);
-                                _logger.LogError(ex, "Error while processing HTTP GET");
-                            }
-                        }
-                        else if (memory.Span.StartsWith("{"u8))
-                        {
-                            try
-                            {
-                                var clientMessage = JsonSerializer.Deserialize<Message>(memory.Span);
+                                    var eventData = line[DataPrefix.Length..];
 
-                                if (clientMessage == null)
-                                {
-                                    _logger.LogError("Could not deserialize message");
-                                    continue;
+                                    if (currentEvent == "send")
+                                    {
+                                        var clientMessage = JsonSerializer.Deserialize<Message>(eventData.Span);
+                                        _logger.LogDebug("Received chat message: {Text}", clientMessage.Content);
+                                        var gen = await _textGen.GenerateTextAsync(_chatData, clientMessage.Content);
+                                        _logger.LogDebug("Generated chat reply: {Text}", gen);
+                                        await SendJson(stream, new Message { Type = "Reply", Content = gen }, cancellationToken);
+                                        cancellationToken.ThrowIfCancellationRequested();
+                                        var speechUrl = await _speechGen.GenerateSpeechUrlAsync(gen);
+                                        _logger.LogDebug("Generated speech URL: {SpeechUrl}", speechUrl);
+                                        await SendJson(stream, new Message { Type = "Speech", Content = speechUrl }, cancellationToken);
+                                    }
+
+                                    currentEvent = null; // Reset the current event
                                 }
 
-                                _logger.LogInformation("Socket Request {Type} {Content}", clientMessage.Type, clientMessage.Content);
-
-                                if (clientMessage.Type == "Send")
-                                {
-                                    _logger.LogDebug("Received chat message: {Text}", clientMessage.Content);
-                                    var gen = await _textGen.GenerateTextAsync(_chatData, clientMessage.Content);
-                                    _logger.LogDebug("Generated chat reply: {Text}", gen);
-                                    await SendJson(stream, new Message { Type = "Reply", Content = gen }, cancellationToken);
-                                    cancellationToken.ThrowIfCancellationRequested();
-                                    var speechUrl = await _speechGen.GenerateSpeechUrlAsync(gen);
-                                    _logger.LogDebug("Generated speech URL: {SpeechUrl}", speechUrl);
-                                    await SendJson(stream, new Message { Type = "Speech", Content = speechUrl }, cancellationToken);
-                                }
+                                lineBuffer.SetLength(0); // Clear the line buffer
+                                processedBytes = lineBreakPosition + 1; // Move to next line
                             }
-                            catch (Exception ex)
+                            else
                             {
-                                _logger.LogError(ex, "Error");
-                                await SendJson(stream, new Message { Type = "error", Content = $"Server exception: {ex.Message}" }, cancellationToken);
+                                // If no line break, write all remaining bytes to lineBuffer
+                                lineBuffer.Write(buffer, processedBytes, bytesRead - processedBytes);
+                                processedBytes = bytesRead; // All bytes processed
                             }
-                        }
-                        else
-                        {
-                            _logger.LogError("Unexpected packet");
                         }
                     }
+
+                    if (lineBuffer.Length > 0)
+                    {
+                        // Handle the last line if it doesn't end with a line break
+                        throw new InvalidOperationException("Unfinished line received: " +
+                                                            Encoding.UTF8.GetString(lineBuffer.GetBuffer().AsMemory(0, (int)lineBuffer.Length).Span));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error");
+                    await SendJson(stream, new Message { Type = "error", Content = $"Server exception: {ex.Message}" }, cancellationToken);
                 }
                 finally
                 {
                     ArrayPool<byte>.Shared.Return(buffer);
+                    await lineBuffer.DisposeAsync();
                 }
             }
             catch (OperationCanceledException)
