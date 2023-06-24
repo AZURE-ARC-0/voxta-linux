a4f1b9e0de62675d355731933936b3ca31843c64
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-06-23 22:45:03 -0400

    Make a UI  that can support more than one page

---
+++ ChatMate.Server/Controllers/PagesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatMate.Server;
+
+[Controller]
+public class PagesController : Controller
+{
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet("/chat")]
+    public IActionResult Chat()
+    {
+        return View();
+    }
+    
+    [HttpGet("/settings")]
+    public IActionResult Settings()
+    {
+        return View();
+    }
+    
+    [HttpGet("/bots")]
+    public IActionResult Bots()
+    {
+        return View();
+    }
+    
+    [HttpGet("/bots/{botId}")]
+    public IActionResult Bot()
+    {
+        return View();
+    }
+}
