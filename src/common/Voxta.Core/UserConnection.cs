6210c3f6a4994876451c429fa441871636f90257
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-07-25 00:38:32 -0400

    Do not crash when disconnecting from vam

--- src/common/Voxta.Core/UserConnection.cs
+++ src/common/Voxta.Core/UserConnection.cs
@@ -66,7 +66,7 @@ public sealed class UserConnection : IAsyncDisposable
                         break;
                 }
             }
-            catch (TaskCanceledException)
+            catch (OperationCanceledException)
             {
                 _logger.LogInformation("Disconnected by cancellation");
             }
