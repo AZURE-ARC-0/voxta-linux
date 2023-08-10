2eba2c7b12a7ec993025200f88dcfc0a430ec45f
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-08-10 00:49:38 -0400

    Memory book editor

---
+++ src/server/Voxta.Server/Controllers/MemoryBooksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Voxta.Abstractions.Model;
+using Voxta.Abstractions.Repositories;
+using Voxta.Server.ViewModels.MemoryBooks;
+
+namespace Voxta.Server.Controllers;
+
+public class MemoryBooksController : Controller
+{
+    private readonly IMemoryRepository _repository;
+
+    public MemoryBooksController(IMemoryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("/characters/{id}/memory")]
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        var memoryBook = await _repository.GetCharacterBookAsync(id);
+        var vm = new MemoryBookViewModel
+        {
+            Id = memoryBook.CharacterId,
+            Name = memoryBook.Name,
+            Description = memoryBook.Description,
+            Items = memoryBook.Items.Select(MemoryItemViewModel.Create).ToList()
+        };
+        return View(vm);
+    }
+
+    [HttpPost("/characters/{id}/memory")]
+    public async Task<IActionResult> Edit([FromRoute] Guid id, MemoryBookViewModel data)
+    {
+        var book = await _repository.GetCharacterBookAsync(id);
+        book.Name = data.Name;
+        book.Description = data.Description;
+        book.Items = data.Items.Select(x => x.ToModel()).ToList();
+        await _repository.SaveBookAsync(book);
+        return RedirectToAction("Edit", new { id });
+    }
+
+    [HttpPost("/characters/{id}/memory/add")]
+    public async Task<IActionResult> AddItem(Guid id)
+    {
+        var memoryBook = await _repository.GetCharacterBookAsync(id);
+        memoryBook.Items.Add(new MemoryItem
+        {
+            Id = Guid.NewGuid(),
+            Keywords = Array.Empty<string>(),
+            Text = "",
+            Weight = 0,
+        });
+        await _repository.SaveBookAsync(memoryBook);
+        return PartialView("_Items", memoryBook.Items.Select(MemoryItemViewModel.Create).ToList());
+    }
+
+    [HttpPost("/characters/{id}/memory/items/{entryId}/remove")]
+    public async Task<IActionResult> RemoveItem(Guid id, Guid entryId)
+    {
+        var memoryBook = await _repository.GetCharacterBookAsync(id);
+        memoryBook.Items.Remove(memoryBook.Items.First(item => item.Id == entryId));
+        await _repository.SaveBookAsync(memoryBook);
+        return PartialView("_Items", memoryBook.Items.Select(MemoryItemViewModel.Create).ToList());
+    }
+
+    [HttpPost("/characters/{id}/memory/reorder")]
+    public async Task<IActionResult> ReorderItems(Guid id, List<int> order)
+    {
+        var memoryBook = await _repository.GetCharacterBookAsync(id);
+        memoryBook.Items = order.Select(index => memoryBook.Items[index]).ToList();
+        await _repository.SaveBookAsync(memoryBook);
+        return PartialView("_Items", memoryBook.Items.Select(MemoryItemViewModel.Create).ToList());
+    }
+}
---
+++ src/server/Voxta.Server/ViewModels/MemoryBooks/MemoryBookViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Voxta.Abstractions.Model;
+
+namespace Voxta.Server.ViewModels.MemoryBooks;
+
+[Serializable]
+public class MemoryBookViewModel
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public List<MemoryItemViewModel> Items { get; set; } = new();
+}
+
+[Serializable]
+public class MemoryItemViewModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Keywords { get; set; }
+    public required int Weight { get; set; }
+    public required string Text { get; set; }
+
+    public MemoryItem ToModel()
+    {
+        return new MemoryItem
+        {
+            Keywords = Keywords.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray(),
+            Text = Text,
+            Weight = Weight,
+            Id = Id,
+        };
+    }
+
+    public static MemoryItemViewModel Create(MemoryItem x)
+    {
+        return new MemoryItemViewModel
+        {
+            Keywords = string.Join(", ", x.Keywords),
+            Text = x.Text,
+            Weight = x.Weight,
+            Id = x.Id,
+        };
+    }
+}
